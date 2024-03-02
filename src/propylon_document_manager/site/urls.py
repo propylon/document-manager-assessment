076d307b76b6abe0f7a311c6ff1c04204e107bba
Eduardo <eduardomnardone@gmail.com>
2024-03-02 13:08:28 +0100

    Register, login and logout

--- src/propylon_document_manager/site/urls.py
+++ src/propylon_document_manager/site/urls.py
@@ -4,18 +4,23 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from rest_framework.authtoken.views import obtain_auth_token
-
 
 # API URLS
 urlpatterns = [
     # API base url
+    path("", RedirectView.as_view(url="/home/")),
     path("api/", include("propylon_document_manager.site.api_router")),
     # DRF auth token
+    path(
+        "api-auth/logout/", RedirectView.as_view(url="/accounts/logout/")
+    ),  # this is just to use django rest, but I'm using allauth
+    path("api-auth/login/", RedirectView.as_view(url="/accounts/login")),
     path("api-auth/", include("rest_framework.urls")),
-    path('accounts/', include('allauth.urls')),
+    path("accounts/", include("allauth.urls")),
     path("auth-token/", obtain_auth_token),
-    path('home/', include('propylon_document_manager.file_versions.urls')),
+    path("home/", include("propylon_document_manager.file_versions.urls")),
 ]
 
 if settings.DEBUG:
