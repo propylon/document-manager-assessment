6aa09602c90d83ef8ae9a88999567de6f56cec31
dearnik1 <nik.khomenko@gmail.com>
2026-06-02 16:24:00 -0500

    handling upload / download by id and hash

--- src/propylon_document_manager/file_versions/api/views.py
+++ src/propylon_document_manager/file_versions/api/views.py
@@ -1,12 +1,42 @@
-from django.shortcuts import render
+from django.http import FileResponse
+from django.shortcuts import get_object_or_404
 
-from rest_framework.mixins import RetrieveModelMixin, ListModelMixin
+from rest_framework.decorators import action
+from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, CreateModelMixin
 from rest_framework.viewsets import GenericViewSet
 
 from ..models import FileVersion
 from .serializers import FileVersionSerializer
 
-class FileVersionViewSet(RetrieveModelMixin, ListModelMixin, GenericViewSet):
+
+class FileVersionViewSet(CreateModelMixin, RetrieveModelMixin, ListModelMixin, GenericViewSet):
     serializer_class = FileVersionSerializer
-    queryset = FileVersion.objects.all()
     lookup_field = "id"
+
+    def get_queryset(self):
+        """Only return file versions belonging to the authenticated user."""
+        return FileVersion.objects.filter(document__user=self.request.user)
+
+    @action(detail=True, methods=["get"])
+    def download(self, request, id=None):
+        """Download the file associated with a specific FileVersion."""
+        file_version = self.get_object()
+        return FileResponse(
+            file_version.file.open(),
+            as_attachment=True,
+            filename=file_version.file_name,
+        )
+
+    @action(detail=False, methods=["get"], url_path=r"cas/(?P<content_hash>[a-f0-9]{64})")
+    def cas_retrieve(self, request, content_hash=None):
+        """Retrieve a file by its SHA-256 content hash (Content Addressable Storage)."""
+        file_version = get_object_or_404(
+            FileVersion,
+            content_hash=content_hash,
+            document__user=request.user,
+        )
+        return FileResponse(
+            file_version.file.open(),
+            as_attachment=True,
+            filename=file_version.file_name,
+        )
