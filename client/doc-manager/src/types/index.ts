b1d5bc154da0c7eda92c66936a0143b6e1e1c4c2
dearnik1 <nik.khomenko@gmail.com>
2026-06-02 18:24:14 -0500

    added User, DocumentData and FileVersionData interfaces

---
+++ client/doc-manager/src/types/index.ts
@@ -0,0 +1,21 @@
+export interface User {
+  email: string;
+  token: string;
+  name?: string;
+}
+
+export interface DocumentData {
+  id: number;
+  url_path: string;
+  created_at: string;
+}
+
+export interface FileVersionData {
+  id: number;
+  document: number;
+  file_name: string;
+  version_number: number;
+  file: string;
+  content_hash: string;
+  created_at: string;
+}
