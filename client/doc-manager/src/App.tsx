a53d54f083f33a2b48e36d9a9c6b41302540c2fc
dearnik1 <nik.khomenko@gmail.com>
2026-06-02 21:10:32 -0500

    Added header and redirect to login page if unauthorized

--- client/doc-manager/src/App.tsx
+++ client/doc-manager/src/App.tsx
@@ -1,12 +1,23 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { MyFilesPage } from "./pages/MyFilesPage";
+import { AppLayout } from "./components/AppLayout";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/files" element={<MyFilesPage />} />
+      <Route
+        path="/files"
+        element={
+          <ProtectedRoute>
+            <AppLayout>
+              <MyFilesPage />
+            </AppLayout>
+          </ProtectedRoute>
+        }
+      />
       <Route path="*" element={<Navigate to="/files" replace />} />
     </Routes>
   );
